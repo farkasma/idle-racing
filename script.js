1bb96425cadb5e1c679f4881fd2cd6b6c1a0c017
farkasma <33036336+farkasma@users.noreply.github.com>
2020-04-21 21:27:37 +0200

    Starting second layer implementation
    Two upgrades have been added to the second layer, but they still need balancing.
    Also refined the prestige system and UI.

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ var track = 100;
 var resetThreshold = track + 100;
 var n = 0;
 var lastUpdate = Date.now();
-var firstReset = false;
 var glitchTrophies = 0;
 
 var engine = {
     bought: 0,
-    mult: 1,
     power: function() {
         return Math.pow(2, mechanic.bought)
     },
@@ -19,7 +17,7 @@ var engine = {
         if (laps < this.cost()) {
             return
         } else {
-            car.speed += mult * this.power();
+            car.speed += this.power();
             laps -= this.cost();
             this.bought += 1;
             document.getElementById("engineCost").textContent = "Cost: " + this.cost().toFixed(2) + " laps";
@@ -49,11 +47,13 @@ var mechanic = {
 var car = {
     speed: 0,
     dist: 0,
+    mult: 1,
+    multGrowth: 0,
 }
 
 function updater() {
     var diff = (Date.now() - lastUpdate) / 1000;
-    car.dist += car.speed * diff;
+    car.dist += car.speed * diff * car.mult;
     lastUpdate = Date.now();
     if (car.dist >= track && car.dist < resetThreshold) {
         laps += 1;
@@ -61,30 +61,50 @@ function updater() {
     };
     document.getElementById("money").textContent = "You have " + laps.toFixed(2) + " lap(s).";
     document.getElementById("pos").textContent = "You are at " + car.dist.toFixed(2) + " meter(s) out of " + track + ".";
-    document.getElementById("speed").textContent = "Your current speed is " + car.speed + " m/s.";
-    if (firstReset == false && car.dist < resetThreshold) {
-        document.getElementById("ascend1").classList.add("inactive");
-    } else {
+    document.getElementById("speed").textContent = "Your current speed is " + car.speed * car.mult + " m/s.";
+    document.getElementById("resetButton").textContent = "Reset the game for " + Math.round(Math.pow(track/100, 2)) + " Glitch Trophies."
+    if (car.dist > resetThreshold) {
         document.getElementById("ascend1").classList.remove("inactive");
+        document.getElementById("resetButton").classList.remove("locked");
+    } else {
+        document.getElementById("resetButton").classList.add("locked");
     };
     document.getElementById("prestigeStats").textContent = "You have " + glitchTrophies + " Glitch Trophie(s).";
+    car.mult += car.multGrowth * diff;
 }
 
 function reset() {
-    glitchTrophies += Math.pow(track/100, 2);
-    car.speed = 0;
-    car.dist = 0;
-    laps = 1;
-    engine.bought = 0;
-    mechanic.bought = 0;
-    document.getElementById("engineCost").textContent = "Cost: " + engine.cost().toFixed(2) + " laps";
-    document.getElementById("engineBought").textContent = "Bought: " + engine.bought;
-    document.getElementById("enginePower").textContent = "Current power is " + engine.power() + ".";
-    document.getElementById("mechanicCost").textContent = "Cost: " + mechanic.cost() + " laps";
-    document.getElementById("mechanicBought").textContent = "Bought: " + mechanic.bought;
-    document.getElementById("prereset").classList.add("inactive");
-    document.getElementById("postreset").classList.remove("inactive");
-    firstReset = true;
+    if (Math.round(Math.pow(track/100, 2)) > 0 && car.dist > resetThreshold) {
+        glitchTrophies += Math.pow(track/100, 2);
+        car.speed = 0;
+        car.dist = 0;
+        laps = 1;
+        engine.bought = 0;
+        mechanic.bought = 0;
+        document.getElementById("engineCost").textContent = "Cost: " + engine.cost().toFixed(2) + " laps";
+        document.getElementById("engineBought").textContent = "Bought: " + engine.bought;
+        document.getElementById("enginePower").textContent = "Current power is " + engine.power() + ".";
+        document.getElementById("mechanicCost").textContent = "Cost: " + mechanic.cost() + " laps";
+        document.getElementById("mechanicBought").textContent = "Bought: " + mechanic.bought;
+        document.getElementById("postreset").classList.remove("inactive");
+        document.getElementById("preresetText").classList.add("inactive");    
+    }
 }
 
+function oilUpgrade() {
+    if (glitchTrophies >= 1) {
+        glitchTrophies -= 1;
+        car.mult *= 1.5;
+        document.getElementById("oilUpgrade").classList.add("inactive");    
+    }
+}
+
+function AIUpgrade() {
+    debugger;
+    if (glitchTrophies >= 2) {
+        glitchTrophies -= 2;
+        car.multGrowth = 0.1;
+        document.getElementById("AIUpgrade").classList.add("inactive");
+    }
+}
 setInterval(updater, 50);
