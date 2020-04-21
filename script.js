2b836c8fe68b60a1de19feeb4dfe709c38394ad8
farkasma <33036336+farkasma@users.noreply.github.com>
2020-04-21 14:00:48 +0200

    Added content
    First layer mostly done, prestige framework ready for second layer stuff.

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 var laps = 1.0;
 var track = 100;
+var resetThreshold = track + 100;
 var n = 0;
 var lastUpdate = Date.now();
+var firstReset = false;
+var glitchTrophies = 0;
 
 var engine = {
     bought: 0,
-    power: 1,
+    mult: 1,
+    power: function() {
+        return Math.pow(2, mechanic.bought)
+    },
     cost: function() {
         return Math.pow(1.1, this.bought)
     },
@@ -13,11 +19,11 @@ var engine = {
         if (laps < this.cost()) {
             return
         } else {
-            car.speed += this.power;
+            car.speed += mult * this.power();
             laps -= this.cost();
             this.bought += 1;
-            document.getElementById("engine").textContent = "Engine: " + this.cost().toFixed(2) + " laps.";
-            document.getElementById("engineButton").textContent = "LVL " + this.bought;
+            document.getElementById("engineCost").textContent = "Cost: " + this.cost().toFixed(2) + " laps";
+            document.getElementById("engineBought").textContent = "Bought: " + this.bought;
         }
     }
 };
@@ -31,12 +37,11 @@ var mechanic = {
         if (laps < this.cost()) {
             return
         } else {
-            engine.power *= 2;
             laps -= this.cost();
             this.bought += 1;
-            document.getElementById("mechanic").textContent = "Mechanic: " + this.cost() + " laps";
-            document.getElementById("mechanicButton").textContent = this.bought + "x";
-            document.getElementById("engineButton").title = "Every bought upgrade increases the speed of your car. Current power is " + this.power + " m/s per upgrade."
+            document.getElementById("mechanicCost").textContent = "Cost: " + this.cost() + " laps";
+            document.getElementById("mechanicBought").textContent = "Bought: " + this.bought;
+            document.getElementById("enginePower").textContent = "Current power is " + engine.power() + ".";
         }
     }
 }
@@ -50,13 +55,36 @@ function updater() {
     var diff = (Date.now() - lastUpdate) / 1000;
     car.dist += car.speed * diff;
     lastUpdate = Date.now();
-    if (car.dist >= track) {
+    if (car.dist >= track && car.dist < resetThreshold) {
         laps += 1;
         car.dist -= track;
     };
     document.getElementById("money").textContent = "You have " + laps.toFixed(2) + " lap(s).";
     document.getElementById("pos").textContent = "You are at " + car.dist.toFixed(2) + " meter(s) out of " + track + ".";
     document.getElementById("speed").textContent = "Your current speed is " + car.speed + " m/s.";
+    if (firstReset == false && car.dist < resetThreshold) {
+        document.getElementById("ascend1").classList.add("inactive");
+    } else {
+        document.getElementById("ascend1").classList.remove("inactive");
+    };
+    document.getElementById("prestigeStats").textContent = "You have " + glitchTrophies + " Glitch Trophie(s).";
+}
+
+function reset() {
+    glitchTrophies += Math.pow(track/100, 2);
+    car.speed = 0;
+    car.dist = 0;
+    laps = 1;
+    engine.bought = 0;
+    mechanic.bought = 0;
+    document.getElementById("engineCost").textContent = "Cost: " + engine.cost().toFixed(2) + " laps";
+    document.getElementById("engineBought").textContent = "Bought: " + engine.bought;
+    document.getElementById("enginePower").textContent = "Current power is " + engine.power() + ".";
+    document.getElementById("mechanicCost").textContent = "Cost: " + mechanic.cost() + " laps";
+    document.getElementById("mechanicBought").textContent = "Bought: " + mechanic.bought;
+    document.getElementById("prereset").classList.add("inactive");
+    document.getElementById("postreset").classList.remove("inactive");
+    firstReset = true;
 }
 
 setInterval(updater, 50);
